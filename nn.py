fbf13bad8e827aa3a00bf572d1c7d2c8649bb86a
Magdalena Wójcik <magdalena.wojcik@gmail.com>
2014-04-13 15:30:14 +0000

    Added momentum technique

--- nn.py
+++ nn.py
@@ -7,6 +7,7 @@ import sys
 NEURONS_FIRST_LAYER = 4
 NEURONS_SECOND_LAYER = 3
 ALPHA = 0.5
+BETA = 0.9
 ETA = 0.1
 
 def code_answer():
@@ -15,11 +16,11 @@ def code_answer():
     
     for i in range(names.shape[0]):
         if names[i] == "Iris-setosa":
-            ans[i][0] = 1
+            ans[i][2] = 1
         elif names[i] == "Iris-versicolor":
             ans[i][1] = 1
         elif names[i] == "Iris-virginica":
-            ans[i][2] = 1
+            ans[i][0] = 1
     
     return ans
 
@@ -29,14 +30,15 @@ def sigmoid(x):
 def sigmoid_gradient(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
-def train():    
+def train(momentum=False):    
     global weights1, weights2, inputs, answer
-    for m in range(50):
+    prev1 = 0
+    prev2 = 0
+    for m in range(100):
         for i in range(inputs.shape[0]):
             net1 = np.dot(inputs[i], weights1)
             s1 = ALPHA*net1
             out1 = sigmoid(s1)
-            print out1
             
             net2 = np.dot(out1, weights2)
             s2 = ALPHA*net2
@@ -44,14 +46,22 @@ def train():
             
             error2 = (answer[i] - out2) * sigmoid_gradient(s2)
             error1 = np.dot(error2, np.transpose(weights2)) * sigmoid_gradient(s1)
-            delta1 = ETA * np.dot(error1[:,None], inputs[i][:,None].T)
+            
+            if(momentum):
+                delta1 = ETA * np.dot(error1[:,None], inputs[i][:,None].T) + BETA * prev1
+                prev1 = delta1
+            else:
+                delta1 = ETA * np.dot(error1[:,None], inputs[i][:,None].T)
+                
             weights1 = weights1 + delta1.T
             
-            delta2 = ETA * np.dot(error2[:,None], out1[:,None].T)
-#            print "weights1: " + str(weights2)
+            if(momentum):
+                delta2 = ETA * np.dot(error2[:,None], out1[:,None].T) + BETA * prev2
+                perv2 = delta2
+            else:
+                delta2 = ETA * np.dot(error2[:,None], out1[:,None].T)
+            
             weights2 = weights2 + delta2.T  
-#            print "weights2: " + str(weights2)
-#            print "delta: " + str(delta2)
             
 def test():
     global weights1, weights2, inputs, answer
@@ -65,8 +75,7 @@ def test():
         out2 = sigmoid(s2)
         
         error_all = np.sum(np.power(out2 - answer[i], 2))
-        f.write("weights1: " + str(weights1) + "\n")
-        f.write("weights2: " + str(weights2) + "\n")
+        f.write("answer: " + str(answer[i]) + "\n")
         f.write("out: " + str(out2) + "\n")
         f.write("error: " + str(error_all) + "\n")
             
@@ -76,7 +85,7 @@ weights1 = np.random.random((inputs.shape[1], NEURONS_FIRST_LAYER))
 weights2 = np.random.random((NEURONS_FIRST_LAYER, NEURONS_SECOND_LAYER))
 answer = code_answer()
 
-f = open('workfile', 'w')
+f = open('output.txt', 'w')
 
 test()
 
